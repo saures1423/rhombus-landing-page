5454e2c0e67b401b7876383d2c709ac024a5596e
Leslie <saures1423@gmail.com>
2025-10-13 19:12:32 +0800

    update fetch

--- src/sections/landing/games.tsx
+++ src/sections/landing/games.tsx
@@ -279,11 +279,14 @@ const Games = () => {
 		}
 
 		try {
-			const response = await fetch('http://localhost:5002/api/users/register', {
-				method: 'POST',
-				headers: { 'Content-Type': 'application/json' },
-				body: JSON.stringify({ username: formData.username }),
-			});
+			const response = await fetch(
+				`${process.env.REACT_APP_SOCKET_URL}/api/users/register`,
+				{
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({ username: formData.username }),
+				},
+			);
 
 			const data = await response.json();
 
